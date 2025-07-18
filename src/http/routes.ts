b03e0f7cc756b70341f480128ee038a192c46b06
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-18 01:27:14 -0300

    feat: create new product search route by id

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,14 +1,16 @@
 import { FastifyInstance } from 'fastify'
 
-import { makeAuthenticateController, makeCreateProductController, makeGetProductsController, makeSignUpController } from '../factories'
+import { makeAuthenticateController, makeCreateProductController, makeGetProductsController, makeSignUpController, makeGetProductIdController } from '../factories'
 
 export async function appRoutes(app: FastifyInstance) {
     const controller = makeCreateProductController()
     const signUpController = makeSignUpController()
     const authenticateController = makeAuthenticateController()
     const getProductsController = makeGetProductsController()
+    const getProductIdController = makeGetProductIdController()
 
     app.get('/products', (req, reply) => getProductsController.handle(req, reply))
+    app.get('/products/:productId', (req, reply) => getProductIdController.handle(req, reply))
     app.post('/products', (req, reply) => controller.handle(req, reply))
     app.post('/users', (req, reply) => signUpController.handle(req, reply))
     app.post('/sessions', (req, reply) => authenticateController.handle(req, reply))
