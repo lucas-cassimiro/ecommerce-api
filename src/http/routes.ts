8ae406dea9c5961678f925fc81ca3ef5042d6e41
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-18 01:03:33 -0300

    refactor: move import to factories re-exports and create a new route getProductsController

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,14 +1,14 @@
 import { FastifyInstance } from 'fastify'
 
-import { makeCreateProductController } from '../factories/make-create-product-controller'
-import { makeSignUpController } from '../factories/make-sign-up-controller'
-import { makeAuthenticateController } from '../factories/make-authenticate-controller'
+import { makeAuthenticateController, makeCreateProductController, makeGetProductsController, makeSignUpController } from '../factories'
 
 export async function appRoutes(app: FastifyInstance) {
     const controller = makeCreateProductController()
     const signUpController = makeSignUpController()
     const authenticateController = makeAuthenticateController()
+    const getProductsController = makeGetProductsController()
 
+    app.get('/products', (req, reply) => getProductsController.handle(req, reply))
     app.post('/products', (req, reply) => controller.handle(req, reply))
     app.post('/users', (req, reply) => signUpController.handle(req, reply))
     app.post('/sessions', (req, reply) => authenticateController.handle(req, reply))
