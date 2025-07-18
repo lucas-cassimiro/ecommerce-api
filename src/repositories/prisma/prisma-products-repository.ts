194db566455599be6a74b58fae626446a3e970eb
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-18 01:04:48 -0300

    feat: implementation findManyPaginated

--- src/repositories/prisma/prisma-products-repository.ts
+++ src/repositories/prisma/prisma-products-repository.ts
@@ -4,6 +4,20 @@ import { ProductsRepository } from '../products-repository'
 export class PrismaProductsRepository implements ProductsRepository {
     constructor(private readonly prisma: PrismaClient) { }
 
+    async findManyPaginated(params: { skip: number; take: number }): Promise<Product[]> {
+        const { skip, take } = params
+
+        const products = await this.prisma.product.findMany({
+            skip,
+            take,
+            orderBy: {
+                createdAt: 'desc',
+            },
+        })
+
+        return products
+    }
+
     async findById(id: number): Promise<Product | null> {
         const product = await this.prisma.product.findUnique({
             where: {
