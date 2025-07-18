cc9d434dc8f118de355a4bbab824f8f4976aca34
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-17 21:48:40 -0300

    feat: add route signUpController

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { FastifyInstance } from 'fastify'
 
 import { makeCreateProductController } from '../factories/make-create-product-controller'
+import { makeSignUpController } from '../factories/make-sign-up-controller'
 
 export async function appRoutes(app: FastifyInstance) {
     const controller = makeCreateProductController()
+    const signUpController = makeSignUpController()
+
     app.post('/products', (req, reply) => controller.handle(req, reply))
+    app.post('/users', (req, reply) => signUpController.handle(req, reply))
 }
