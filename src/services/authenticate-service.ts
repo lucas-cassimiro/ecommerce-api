52204c6b7453dc3c39212e1680558a66bc0c343a
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-17 23:02:00 -0300

    feat: create authentication with AuthenticateService

---
+++ src/services/authenticate-service.ts
@@ -0,0 +1,46 @@
+import { UsersRepository } from '../repositories/users-repository'
+import { User } from '../generated/prisma'
+import { TokenGenerator } from '../domain/protocols/token-generator'
+import { HashComparer } from '../domain/criptography/hash-comparer'
+
+interface AuthenticateRequest {
+    email: string
+    password: string
+}
+
+interface AuthenticateResponse {
+    user: User
+    token: string
+}
+
+export class AuthenticateService {
+    constructor(
+        private readonly usersRepository: UsersRepository,
+        private readonly hashComparer: HashComparer,
+        private readonly tokenGenerator: TokenGenerator
+    ) { }
+
+    async execute({
+        email,
+        password,
+    }: AuthenticateRequest): Promise<AuthenticateResponse> {
+        const user = await this.usersRepository.findByEmail(email)
+
+        if (!user) {
+            throw new Error()
+        }
+
+        const doesPasswordMatches = await this.hashComparer.compare(password, user.passwordHash)
+
+        if (!doesPasswordMatches) {
+            throw new Error()
+        }
+
+        const token = await this.tokenGenerator.generate({ id: user.id, email: user.email })
+
+        return {
+            user,
+            token
+        }
+    }
+}
