cac7653d684796ac4a4e859e85c6388ecfe7642a
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-17 23:01:29 -0300

    feat: create JwtAdapter

---
+++ src/infra/criptography/jwt-adapter.ts
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken'
+import { TokenGenerator } from '../../domain/protocols/token-generator'
+
+export class JwtAdapter implements TokenGenerator {
+    constructor(
+        private readonly secret: string,
+        private readonly expiresIn: string
+    ) { }
+
+    async generate(payload: object): Promise<string> {
+        const token = jwt.sign(payload, this.secret, { expiresIn: this.expiresIn })
+        return token
+    }
+}
