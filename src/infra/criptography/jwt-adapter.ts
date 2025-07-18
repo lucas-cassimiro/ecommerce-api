b0e4440ac43b20ffef1eb3f618864e98e77cf388
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-18 00:00:52 -0300

    refactor: change the type of secret and expiresIn to the types jwt Secret and SignOptions expires in

--- src/infra/criptography/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter.ts
@@ -3,12 +3,13 @@ import { TokenGenerator } from '../../domain/protocols/token-generator'
 
 export class JwtAdapter implements TokenGenerator {
     constructor(
-        private readonly secret: string,
-        private readonly expiresIn: string
+        private readonly secret: jwt.Secret,
+        private readonly expiresIn: jwt.SignOptions['expiresIn']
     ) { }
 
     async generate(payload: object): Promise<string> {
         const token = jwt.sign(payload, this.secret, { expiresIn: this.expiresIn })
+
         return token
     }
 }
