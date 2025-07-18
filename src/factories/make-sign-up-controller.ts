945a80aaff639fe0d87315305702187616bb7c64
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-17 23:00:53 -0300

    refactor: bcryptAdapter instance and sending to signUpService

--- src/factories/make-sign-up-controller.ts
+++ src/factories/make-sign-up-controller.ts
@@ -1,11 +1,14 @@
 import { SignUpController } from '../http/controllers/sign-up-controller'
+import { BcryptAdapter } from '../infra/criptography/bcrypt-adapter'
 import { prisma } from '../lib/prisma'
 import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
 import { SignUpService } from '../services/sign-up-service'
 
 export const makeSignUpController = () => {
+    const salt = 12
+    const bcryptAdapter = new BcryptAdapter(salt)
     const usersRepository = new PrismaUsersRepository(prisma)
-    const signUpService = new SignUpService(usersRepository)
+    const signUpService = new SignUpService(usersRepository, bcryptAdapter)
     const signUpController = new SignUpController(signUpService)
 
     return signUpController
