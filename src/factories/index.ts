6ea7087eb1c6fbb2ad362c64aeee2f8c1863179f
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-18 01:01:24 -0300

    feat: re-export

---
+++ src/factories/index.ts
@@ -0,0 +1,4 @@
+export * from './make-authenticate-controller'
+export * from './make-create-product-controller'
+export * from './make-get-products-controller'
+export * from './make-sign-up-controller'
