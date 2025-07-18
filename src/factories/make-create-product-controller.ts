45520312b31b3446e7fa9222ab70a14f603c3fba
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-17 21:16:22 -0300

    feat: create a factory makeCreateProductController

---
+++ src/factories/make-create-product-controller.ts
@@ -0,0 +1,15 @@
+import { CreateProductController } from '../http/controllers/create-product-controller'
+import { type Controller } from '../http/protocols/controller'
+import { prisma } from '../lib/prisma'
+import { PrismaProductsRepository } from '../repositories/prisma/prisma-products-repository'
+import { PrismaStocksRepository } from '../repositories/prisma/prisma-stocks-repository'
+import { CreateProductService } from '../services/create-product-service'
+
+export const makeCreateProductController = () => {
+    const productsRepository = new PrismaProductsRepository(prisma)
+    const stocksRepository = new PrismaStocksRepository(prisma)
+    const createProductService = new CreateProductService(productsRepository, stocksRepository, prisma)
+    const createProductController = new CreateProductController(prisma, createProductService)
+
+    return createProductController
+}
