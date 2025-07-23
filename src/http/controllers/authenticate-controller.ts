45b8a99d6e9f8fa73bcd19ef617f10bd35bd7c50
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-22 23:01:34 -0300

    feat: get accessToken and refreshToken and set refreshToken

--- src/http/controllers/authenticate-controller.ts
+++ src/http/controllers/authenticate-controller.ts
@@ -15,15 +15,23 @@ export class AuthenticateController {
         try {
             const { email, password } = authenticateBodySchema.parse(request.body)
 
-            const { user, token } = await this.authenticateService.execute({
+            const { user, accessToken, refreshToken } = await this.authenticateService.execute({
                 email,
                 password
             })
 
-            return response.status(201).send({
-                user,
-                token
-            })
+            return response
+                .status(200)
+                .setCookie('refreshToken', refreshToken, {
+                    path: '/',
+                    secure: false,
+                    sameSite: 'lax',
+                    httpOnly: true,
+                })
+                .send({
+                    user,
+                    accessToken,
+                })
         } catch (err) {
             console.error(err)
             return response.status(400).send({ message: 'Erro ao autenticar usuário', error: err })
