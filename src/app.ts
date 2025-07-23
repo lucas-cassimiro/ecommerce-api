b4292d25e650d9e038eebf0c88ae99ddf9998d0f
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-22 22:57:32 -0300

    feat: register fastifyCookie and pass secret

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import { fastifyCookie } from '@fastify/cookie'
 import { appRoutes } from './http/routes'
 
 export const app = fastify()
 
 app.register(cors, {
-    origin: true
+    origin: true,
+    credentials: true
+})
+
+app.register(fastifyCookie, {
+    secret: 'mysecretkey'
 })
 
 app.register(appRoutes)
+
