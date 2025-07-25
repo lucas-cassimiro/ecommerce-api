1124d4d01440ad304444e5796e80af16d3d1e9bd
lucas-cassimiro <lucascassimiro545@hotmail.com>
2025-07-25 17:50:36 -0300

    refactor: CreateProductService receives new service dependency from MinioStorage storage

--- src/factories/make-create-product-controller.ts
+++ src/factories/make-create-product-controller.ts
@@ -1,13 +1,17 @@
+import { Client } from 'minio'
 import { CreateProductController } from '../http/controllers/create-product-controller'
+import { MinioStorage } from '../infra/storage/minio-storage'
 import { prisma } from '../lib/prisma'
 import { PrismaProductsRepository } from '../repositories/prisma/prisma-products-repository'
 import { PrismaStocksRepository } from '../repositories/prisma/prisma-stocks-repository'
 import { CreateProductService } from '../services/create-product-service'
+import { minioClient } from '../lib/minio'
 
 export const makeCreateProductController = () => {
+    const minioStorage = new MinioStorage(minioClient, 'products')
     const productsRepository = new PrismaProductsRepository(prisma)
     const stocksRepository = new PrismaStocksRepository(prisma)
-    const createProductService = new CreateProductService(productsRepository, stocksRepository, prisma)
+    const createProductService = new CreateProductService(productsRepository, stocksRepository, prisma, minioStorage)
     const createProductController = new CreateProductController(createProductService)
 
     return createProductController
