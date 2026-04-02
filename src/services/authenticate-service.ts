40f348c7ff85c1fc3be76f41cf9b6f4be6332a01
lucas-cassimiro <lucascassimiro545@hotmail.com>
2026-04-02 17:37:36 -0300

    refactor: add cookie adapter

--- src/services/authenticate-service.ts
+++ src/services/authenticate-service.ts
@@ -1,54 +1,68 @@
-import { UsersRepository } from '../repositories/users-repository'
-import { User } from '../generated/prisma'
-import { TokenGenerator } from '../domain/protocols/token-generator'
-import { HashComparer } from '../domain/criptography/hash-comparer'
+import { UsersRepository } from "../repositories/users-repository";
+import { User } from "../generated/prisma";
+import { TokenGenerator } from "../domain/protocols/token-generator";
+import { HashComparer } from "../domain/criptography/hash-comparer";
+import { CookieHandler } from "../domain/protocols/cookie-adapter";
 
 interface AuthenticateRequest {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
 interface AuthenticateResponse {
-    user: User
-    accessToken: string
-    refreshToken: string
+  user: User;
+  accessToken: string;
+  refreshToken: string;
 }
 
 export class AuthenticateService {
-    constructor(
-        private readonly usersRepository: UsersRepository,
-        private readonly hashComparer: HashComparer,
-        private readonly tokenGenerator: TokenGenerator
-    ) { }
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly tokenGenerator: TokenGenerator,
+    private readonly cookieAdapter: CookieHandler
+  ) {}
 
-    async execute({
-        email,
-        password,
-    }: AuthenticateRequest): Promise<AuthenticateResponse> {
-        const user = await this.usersRepository.findByEmail(email)
+  async execute({
+    email,
+    password,
+  }: AuthenticateRequest): Promise<AuthenticateResponse> {
+    const user = await this.usersRepository.findByEmail(email);
 
-        if (!user) {
-            throw new Error()
-        }
-
-        const doesPasswordMatches = await this.hashComparer.compare(password, user.passwordHash)
-
-        if (!doesPasswordMatches) {
-            throw new Error()
-        }
-
-        const userData = {
-            id: user.id,
-            email: user.email
-        }
-
-        const accessToken = await this.tokenGenerator.generateAccessToken(userData)
-        const refreshToken = await this.tokenGenerator.generateRefreshToken(userData)
-
-        return {
-            user,
-            accessToken,
-            refreshToken
-        }
+    if (!user) {
+      throw new Error();
     }
+
+    const doesPasswordMatches = await this.hashComparer.compare(
+      password,
+      user.passwordHash
+    );
+
+    if (!doesPasswordMatches) {
+      throw new Error();
+    }
+
+    const userData = {
+      id: user.id,
+      email: user.email,
+    };
+
+    const accessToken = await this.tokenGenerator.generateAccessToken(userData);
+    const refreshToken = await this.tokenGenerator.generateRefreshToken(
+      userData
+    );
+
+    this.cookieAdapter.setCookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      path: "/",
+      secure: false,
+      sameSite: "lax",
+    });
+
+    return {
+      user,
+      accessToken,
+      refreshToken,
+    };
+  }
 }
