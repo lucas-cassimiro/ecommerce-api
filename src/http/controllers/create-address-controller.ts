20c022d7c6ba21d63890a2918ad08c343774710e
lucas-cassimiro <lucascassimiro545@hotmail.com>
2026-04-02 17:25:29 -0300

    feat: create AddressController

---
+++ src/http/controllers/create-address-controller.ts
@@ -0,0 +1,10 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateAddressesService } from "../../services/create-addresses-service";
+
+export class CreateAddressController {
+    constructor(private readonly createAddressService: CreateAddressesService) {}
+
+    async handle(request: FastifyRequest, response: FastifyReply) {
+        
+    }
+}
