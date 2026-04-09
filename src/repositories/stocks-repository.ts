25743a80950b6ab218777bf213886388776551bb
lucas-cassimiro <lucascassimiro545@hotmail.com>
2026-04-09 08:52:38 -0300

    refactor: move repositories from src/repositories to src/infra/db/prisma/repositories

--- src/repositories/stocks-repository.ts
+++
@@ -1,6 +0,0 @@
-import { Prisma, Stock } from '../generated/prisma'
-
-export interface StocksRepository {
-    findByProductId: (productId: number) => Promise<Stock | null>
-    create(data: Prisma.StockUncheckedCreateInput, tx?: Prisma.TransactionClient): Promise<Stock>
-}
--- src/repositories/users-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma, User } from '../generated/prisma'
-
-export interface UsersRepository {
-    findById: (id: number) => Promise<User | null>
-    findByEmail: (email: string) => Promise<User | null>
-    create: (data: Prisma.UserCreateInput) => Promise<User>
-}
