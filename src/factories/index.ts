1e66cd80e01d3f649cd33f2f96fa90c740c2b5a1
lucas-cassimiro <lucascassimiro545@hotmail.com>
2026-04-09 08:49:49 -0300

    refactor: move factories from src/factories to src/main/factories

--- src/factories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './make-authenticate-controller'
-export * from './make-create-product-controller'
-export * from './make-get-products-controller'
-export * from './make-sign-up-controller'
-export * from './make-get-product-id-controller'
-export * from './make-get-user-profile-controller'
