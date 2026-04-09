8e6d1a88a6a4ec240e74465626ce00ef70466305
lucas-cassimiro <lucascassimiro545@hotmail.com>
2026-04-09 13:13:56 -0300

    feat: add repositories to infra

---
+++ src/infra/db/prisma/repositories/addresses-repository.ts
@@ -0,0 +1,7 @@
+import { Prisma, Address } from "../../../../generated/prisma"
+
+export interface AddressesRepository {
+  create(data: Prisma.AddressUncheckedCreateInput): Promise<Address>;
+  // update(id: number, newAddress: Address): Promise<void>
+  // delete(id: number): Promise<void>
+}
