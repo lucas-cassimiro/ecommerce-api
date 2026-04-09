dd18eaff2cb4ca8b34567fb1806bc5578b672c5d
lucas-cassimiro <lucascassimiro545@hotmail.com>
2026-04-09 08:17:48 -0300

    refactor: path to appRoutes has been changed

--- src/app.ts
+++ src/app.ts
@@ -3,29 +3,29 @@ import cors from '@fastify/cors'
 import { fastifyCookie } from '@fastify/cookie'
 import multipart from '@fastify/multipart'
 
-import { appRoutes } from './http/routes'
+import { appRoutes } from './infra/http/routes'
 
 export const app = fastify()
 
 app.register(cors, {
-    origin: true,
-    credentials: true
+  origin: true,
+  credentials: true,
 })
 
 app.register(fastifyCookie, {
-    secret: 'mysecretkey'
+  secret: 'mysecretkey',
 })
 
 app.register(multipart, {
-    limits: {
-        fieldNameSize: 100,
-        fieldSize: 100,
-        fields: 10,        
-        fileSize: 1000000, 
-        files: 1,         
-        headerPairs: 2000
-    },
-    attachFieldsToBody: false 
+  limits: {
+    fieldNameSize: 100,
+    fieldSize: 100,
+    fields: 10,
+    fileSize: 1000000,
+    files: 1,
+    headerPairs: 2000,
+  },
+  attachFieldsToBody: false,
 })
 
 app.register(appRoutes)
